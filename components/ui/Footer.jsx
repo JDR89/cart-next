9537503b5a4c95a7b8337e23593edb70cbbcd75d
JDR89 <joaquin.romero89@gmail.com>
2023-11-13 20:16:01 -0300

    footer

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="">
-    <footer className="footer footer-center  p-4 bg-base-300 text-base-content  ">
+    <footer className="footer footer-center  p-4 bg-base-300 text-base-content mt-[1rem]">
       <aside>
         <p>Copyright © 2023 - All right reserved by @JDR89</p>
       </aside>
