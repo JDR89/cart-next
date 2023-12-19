acebe88a4a8312d32d0c6127925107851a1c5564
JDR89 <joaquin.romero89@gmail.com>
2023-12-19 18:22:27 -0300

    arreglo config.js

--- firebase/config.js
+++ firebase/config.js
@@ -7,12 +7,12 @@ import {getAuth,GoogleAuthProvider} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCef9iCoafXk7U7QiRVUlLSXssA4SrFCIM",
-  authDomain: "codernextapp.firebaseapp.com",
-  projectId: "codernextapp",
-  storageBucket: "codernextapp.appspot.com",
-  messagingSenderId: "873078178236",
-  appId: "1:873078178236:web:09b10cebf1182f34fa6bb9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
